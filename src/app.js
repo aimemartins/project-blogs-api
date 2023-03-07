c8dc002ffda69bfd87382c12a166566197ef555b
AIMÊ <aimemartins@hotmail.com>
2023-03-07 12:05:08 -0300

    feat: endpoint POST/login

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const { loginRouter } = require('./routers/index');
 // ...
 
 const app = express();
@@ -12,6 +12,7 @@ app.get('/', (_request, response) => {
 app.use(express.json());
 
 // ...
+app.use('/login', loginRouter);
 
 // É importante exportar a constante `app`,!
 // para que possa ser utilizada pelo arquivo `src/server.js`
